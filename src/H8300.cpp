6c08ba7727d0eb66f5a90db2feca29c4d8bd7fba
Barret <barret.klics@gmail.com>
2025-01-28 08:08:23 -0700

    test udp

--- src/H8300.cpp
+++ src/H8300.cpp
@@ -11,12 +11,13 @@
 #include <errno.h>
 #include <unistd.h>
 #include <sys/time.h>
-
+#include <arpa/inet.h>
 
 
 H8300::H8300(){
     printf("H8/300 has been created");
     //debugSerialPort();
+    if (IRMode == 2) debugPipe();
 }
 
 H8300::~H8300(){
@@ -122,6 +123,11 @@ u8 H8300::handleSPICompatibility(u8& IRCmd, u8& val, u32&pos, bool& last){
 
 //Serial Forwarding
 u8 H8300::handleSPISerial(u8& IRCmd, u8& val, u32&pos, bool& last){
+    if (fd == -1){
+        configureSerialPort();
+
+
+    }
     switch (IRCmd)
     {
     case 0x00: // pass-through. This should never occur and should (and is) intercepted by NDSCart.cpp
@@ -138,6 +144,7 @@ u8 H8300::handleSPISerial(u8& IRCmd, u8& val, u32&pos, bool& last){
 	}
 	else{
 		u8 data = (unsigned char)buf[pos-2];
+
 		return data;
 	}
 	return 0x00;
@@ -169,22 +176,95 @@ u8 H8300::handleSPISerial(u8& IRCmd, u8& val, u32&pos, bool& last){
 
 //TODO Implement UDP mode. For now, just set it as compatibility mode.
 u8 H8300::handleSPIUDP(u8& IRCmd, u8& val, u32&pos, bool& last){
-    if (pos == 0)
-    {
-        IRCmd = val;
-        return 0;
-    }
+
+    printf("IRCmd: %d   val: 0x%02x   pos: %ld   last %d\n", IRCmd, val ^ 0xaa, pos, last);
     switch (IRCmd)
     {
-    case 0x00: // should not happen
-        return 0xff;
+    case 0x00: // pass-through. This should never occur and should (and is) intercepted by NDSCart.cpp
+        return 0xFF;
+    case 0x01:
+	//This should never occur. Pos 0 is used to "load" the IRCmd in the first place.
+	if (pos == 0){
+		return 0x00;
+	}
+	if (pos == 1){
+		//memset(buf, 0, sizeof(buf)); //needed???
+		recvLen = readUDP();
+		return recvLen;
+	}
+	else{
+		u8 data = (unsigned char)buf[pos-2];
 
-    case 0x08: // ID
-        return 0xAA;
+        printf("ATE 0x%02x at POS %d\n", data, pos);
+		return data;
+	}
+	return 0x00;
+
+    case 0x02:
+	if (pos == 0){
+	    recvLen = 0;
+	}
+	else{
+	    sendBuf[pos-1] = (u8) val; //Load the spi packet into the buffer;
     }
 
-    return 0;
+	if (last == 1){
+		u8 sendLen = pos;
+		sendUDP(sendLen);
+    }
+	return 0x00;
+    case 0x08: //Maybe there is a better place to handle this, but I do not really see any issue.
+	if (fd <0){
+		//printf("Configuring port during IR init sequence\n");
+		//configureSerialPort();
+	}
+        return 0xAA; // Version check.
+    }
+    return 0x00;
 }
+
+
+int H8300::readUDP(){
+    char tempBuf[0xB8];
+    socklen_t addrLen = sizeof(out_ip);
+
+    int len = recvfrom(sock, tempBuf, sizeof(tempBuf), 0, (struct sockaddr*)&out_ip, &addrLen);
+    if (len < 0) {
+        perror("UDP receive error: ");
+        return 0;
+    }
+
+    recvLen = len;
+    memcpy(buf, tempBuf, len); // Copy the received data into buf
+
+    printf("\nReceived %d Bytes via UDP:\n", recvLen);
+    for (int i = 0; i < recvLen; i++) {
+        printf("0x%02x ", (u8)buf[i] ^ 0xaa);
+    }
+    printf("\n");
+    return recvLen;
+
+
+
+}
+
+int H8300::sendUDP(u8 len){
+    printf("\nSending %d Bytes via UDP:\n", len);
+    for (int i = 0; i < len; i++) {
+        printf("0x%02x ", (u8)sendBuf[i] ^ 0xaa);
+    }
+    printf("\n");
+
+    int sent = sendto(sock, sendBuf, len, 0, (struct sockaddr*)&out_addr, sizeof(out_addr));
+    if (sent < 0) {
+        perror("UDP send error: ");
+        return -1;
+    }
+    return sent;
+
+}
+
+
 
 /////////////////////////////////////////////////////////////////////
 int H8300::configureSerialPort(){
@@ -266,22 +346,22 @@ int H8300::readSerialPort(){
 
 	recvLen = pointer;
 	if (recvLen == 0) return 0;
-    /*
+
 	printf("\nRecieved %d Bytes \n", recvLen);
 	for (int i = 0; i < recvLen; i++){
 		printf("0x%02x ", (u8)buf[i] ^ 0xaa);
 	}
-	printf("\n");*/
+	printf("\n");
 	return recvLen;
 }
 
 
 int H8300::sendSerialPort(u8 sendLen){
-/*	printf("\n Sending %d Bytes: \n", sendLen);
+	printf("\n Sending %d Bytes: \n", sendLen);
 	for (int i = 0; i < sendLen; i++){
 		printf("0x%02x ", (u8)sendBuf[i] ^ 0xaa);
 	}
-	printf("\n");*/
+	printf("\n");
 
 	if ((u8)sendBuf[0] == 94){
         usleep(10000);  /*Necessary hack. Could be a slower time possibly, but is less than one frame length. This needs to halt
@@ -292,4 +372,118 @@ int H8300::sendSerialPort(u8 sendLen){
 		perror("err: ");
 	}
 	return 0;
+}
+
+
+
+
+
+
+
+void H8300::debugPipe(){
+	int option = 0;
+	while(option != 9){
+		printf("\n\nWelcome to the H8300 UDP debugger.\n");
+		printf("1. cfg1 \n");
+		printf("2. cfg2\n");
+		printf("3. send ping\n");
+        printf("4. rx loop\n");
+        printf("9. exit\n");
+		scanf("%d", &option);
+		if (option ==1){
+
+            in_ip = "127.0.0.1";
+            in_port = 12345;
+            out_ip = "127.0.0.1";
+            out_port = 12346;
+            startUDP();
+        }
+		else if (option == 2){
+
+            in_ip = "127.0.0.1";
+            in_port = 12346;
+            out_ip = "127.0.0.1";
+            out_port = 12345;
+
+            startUDP();
+        }
+
+        else if (option == 9){
+            printf("continuing");
+            break;
+        }
+    }
+}
+
+u8 H8300::startUDP(){
+    char buffer[256];
+
+    socklen_t addr_len = sizeof(in_addr);
+
+
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+
+    struct timeval timeout;
+    timeout.tv_sec = 3;
+    timeout.tv_usec = 0;
+    if (setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0) {
+        perror("setsockopt");
+        exit(1);
+    }
+
+
+
+    //INCOMING
+    in_addr.sin_family = AF_INET;
+    in_addr.sin_port = htons(in_port);
+    in_addr.sin_addr.s_addr = inet_addr(in_ip);
+    if (bind(sock, (struct sockaddr *)&in_addr, sizeof(in_addr)) < 0){
+        perror("Failed to bind");
+        exit(1);
+    }
+
+
+    // OUTGOING
+    out_addr.sin_family = AF_INET;
+    out_addr.sin_port = htons(out_port);
+    out_addr.sin_addr.s_addr = inet_addr(out_ip);
+
+
+    printf("Program running on incoming port %d, sending messages to %s:%d\n", in_port, out_ip, out_port);
+
+
+    /*
+    while (1) {
+        // Send "ping"
+        strcpy(buffer, "ping");
+        sendto(sock, buffer, strlen(buffer), 0, (struct sockaddr *)&out_addr, addr_len);
+
+        // Receive response
+        int received = recvfrom(sock, buffer, sizeof(buffer) - 1, 0, (struct sockaddr *)&in_addr, &addr_len);
+        if (received < 0) {
+            perror("recvfrom timeout");
+            printf("Timeout: No response from the other program.\n");
+        } else {
+            buffer[received] = '\0'; // Null-terminate the received message
+            printf("Received: %s\n", buffer);
+        }
+
+        sleep(1); // Wait before sending the next message
+    }*/
+
+   // close(sock);
+
+
+
+    /*int flags = fcntl(sock, F_GETFL, 0);  // Get current flags
+    if (flags == -1) return -1;             // Error checking
+
+    return fcntl(sock, F_SETFL, flags | O_NONBLOCK);  // Set non-blocking*/
+    return 0;
+
+
 }
--- src/H8300.h
+++ src/H8300.h
@@ -11,6 +11,7 @@
 #include <variant>
 
 #include "types.h"
+#include <netinet/in.h>
 
 
 using namespace melonDS;
@@ -27,13 +28,23 @@ public:
 	int readSerialPort();
 	int sendSerialPort(u8 len);
 
+
+
+
+
+    //PIPE stuff
+    void debugPipe();
+    u8 startUDP();
+
+    int sendUDP(u8 len);
+    int readUDP();
 private:
     u8 IRMode = 1;
-
 
 	u8 handleSPICompatibility(u8& IRCmd, u8& val, u32&pos, bool& last); //Mode 0
 	u8 handleSPISerial(u8& IRCmd, u8& val, u32&pos, bool& last);        //Mode 1
 	u8 handleSPIUDP(u8& IRCmd, u8& val, u32&pos, bool& last);           //Mode 2
+
 
     long long getTimeUS();
 
@@ -44,6 +55,21 @@ private:
 	u8 recvLen = 0;
 	char buf[0xB8];
 	char sendBuf[0xB8];
+
+    //UDP stuff
+    const char* in_ip;
+    const char* out_ip;
+    int in_port;
+    int out_port;
+
+    struct sockaddr_in in_addr;
+    struct sockaddr_in out_addr;
+
+
+
+    int sock;
+
+
 };
 
 #endif
